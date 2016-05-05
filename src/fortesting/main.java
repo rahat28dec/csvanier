0ba12cf2e98780257208026b9b123e6dd3bfc67f
Raymart De Guzman <tramyardg@gmail.com>
2016-05-05 18:40:04 -0400

    OOP update: bubble sort!

--- src/fortesting/main.java
+++ src/fortesting/main.java
@@ -5,27 +5,19 @@
  */
 package fortesting;
 
+import java.util.Arrays;
+
 /**
  *
  * @author RAYMARTHINKPAD
  */
 public class main {
-    public static String formatStr(String str) {
-        return str.substring(0, 0).toLowerCase();
-    }
-    
-    public static boolean validatePhNum(String phNumber) {
-        return phNumber.length()==10 && phNumber.matches(".*\\d");
-    }
     public static void main(String[] args) {
-//        String insert = "Insert";
-//        System.out.println("formated string: " + formatStr(insert));
-//        
-//        String phNumber = "123456789z";
-//        System.out.println((phNumber.length()));
-//        System.out.println("is valid : " + validatePhNum(phNumber));
-        int[] arr = {5,6,4,2,1,7,8,9};
-        findElem(arr, 2);
+        int[] arr = {5,9,4,2,1,7};
+        //findElem(arr, 2);
+        System.out.println("original array: \t" + Arrays.toString(arr));
+
+        bubblesort(arr);
     }
     
     public static void findElem(int[] arr, int find) {
@@ -33,8 +25,71 @@ public class main {
             System.out.println("index: "+i);
             if(arr[i]==find) {
                 System.out.println("Found!");
-//                break;
+                break;
             }
         }
+    }
+    
+    public static int[] bubblesort(int[] arr) {
+        int lastPos;
+        int index;
+        // minus 1 since we adding + 1 at index
+        // also to avoid out of bounds error
+        for ( lastPos = arr.length - 1; lastPos >= 0; lastPos--) {
+            // we are basically shifting larger
+            // elements to the right
+            // after the shift decrement lastPos by 1
+            // so we don't touch those elements
+            // anymore, we sort what we have left
+            // controlled by the lastPos index
+            // each time we pass the array
+            // we are shifting the larger elements
+            // to the right until until we reach
+            // 0
+            
+            // example: { 5, 9, 4, 2, 1, 7 }
+            
+            // first iteration is
+            //  from 0 to 4
+            //    we shift #9 all the way to the right
+            //      { 5, 4, 2, 1, 7, 9 }
+            
+            // second iteration is
+            //  from 0 to 3
+            //    we shift #5 all the way to the right
+            //      { 4, 2, 1, 5, 7, 9 }
+            
+            // third iteration is 
+            //  from 0 to 2
+            //    we shift #4 all the way to the right
+            //      { 2, 1, 4, 5, 7, 9 }
+
+            // fourth iteration is 
+            //  from 0 to 1
+            //    we shift #2 all the way to the right
+            //      { 1, 2, 4, 5, 7, 9 }
+            
+            // after each iteration
+            // we are shifting the larger elements 
+            // to the right
+            // so we are left with small range each pass
+            
+            for ( index = 0; index < lastPos; index++) {
+                
+                
+                
+                int temp;
+                boolean cond = false;
+                if (arr[index] > arr[index + 1]) {
+                    temp = arr[index]; // is holding the larger elem
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = temp;
+                    cond = true;
+                }
+                System.out.println("index:" + index + " " + Arrays.toString(arr) + " swap " + cond);
+            }
+
+        }
+        return arr;
     }
 }
--- src/fortesting/sorting/BubbleSort.java
+++ src/fortesting/sorting/BubbleSort.java
@@ -13,10 +13,10 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {3,2,7,8,9,1};
+        int[] arr = {3,2,9,7,6,8};
         System.out.println("original array: "+Arrays.toString(arr));
 
-        System.out.println(Arrays.toString(bubblesort(arr)));
+        System.out.println("result: "+Arrays.toString(bubblesort(arr)));
     }
     /**
      * larger values “bubble” toward the end 
@@ -26,12 +26,13 @@ public class BubbleSort {
      */
     public static int[] bubblesort(int[] arr) {
         
-        int temp;
+        
         for( int i = 0; i < arr.length-1; i++) {
             // if current element is larger
             // than the next elem, swap
             // otherwise do nothing increment index
             // the larger element is shifted to the right
+            int temp;
             boolean cond = false;
             if(arr[i]>arr[i+1]) {
 //                System.out.println("arr[index]>arr[index+1]:\t" + arr[i] + ">" + arr[i+1]);
@@ -40,29 +41,19 @@ public class BubbleSort {
                 arr[i+1]=temp;
                 cond=true;
             }
-            System.out.println("index:" + i + " " + Arrays.toString(arr) + " swap " + cond);
+//            System.out.println("index:" + i + " " + Arrays.toString(arr) + " swap " + cond);
         }
+        
+        for(int i = arr.length-1; i > 0; i--) {
+            int temp;
+            if(arr[i]<arr[i-1]) {
+                temp = arr[i];
+                arr[i]=arr[i-1];
+                arr[i-1]=temp;
+            }
+        } 
         return arr;
     }
 }
 
-/**
- * 7 2 3 8 9 1
- * int i = 0;
- * array length - 1 = 5
- * 0 < 5
- * arr[0]:7 > arr[0+1]:2
- * temp = arr[0]:7 -> gets the larger elem
- * arr[0] = arr[0+1] -> swap
- * arr[0]   is now 2
- * arr[0+1] = temp:7
- * arr[1] is now 7 
- * i++
- * 
- * i = 1
- * 1 < 5
- * arr[1]:7 > arr[1+1]:3
- * temp = arr[1]:7
- * arr[1] = arr[1+1] -> swap 7 and 3
- * arr[2] = temp:7
- */
+
